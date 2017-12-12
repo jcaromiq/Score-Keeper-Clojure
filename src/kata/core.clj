dacd70a1011a61837fb5d68ef554ff5f24531d5d
Joaquin Caro <me@joaquin-caro.es>
2017-12-12 08:42:15 +0100

    Allow teamA to score

--- src/kata/core.clj
+++ src/kata/core.clj
@@ -1,3 +1,13 @@
 (ns kata.core)
 
-(defn score-team-a [] 1)
+(defn score-team-a
+  ([] {:teamA 1 :teamB 0})
+   ([score] (assoc score :teamA (+ (:teamA score) 1))))
+
+(defn score-team-a-2
+  ([] {:teamA 2 :teamB 0})
+  ([score] (assoc score :teamA (+ (:teamA score) 2))))
+
+(defn score-team-a-3
+  ([]  {:teamA 3 :teamB 0})
+  ([score] (assoc score :teamA (+ (:teamA score) 3))))
--- test/kata/core_should.clj
+++ test/kata/core_should.clj
@@ -4,10 +4,10 @@
 
 (facts
   "About score keeper"
-  (fact
-    "Team A scores a point "
-    (score-team-a) => 1
+  (fact  "Team A scores a point" (score-team-a) => {:teamA 1 :teamB 0})
+  (fact  "Team A scores two points" (score-team-a-2) => {:teamA 2 :teamB 0})
+  (fact  "Team A scores three points" (score-team-a-3) => {:teamA 3 :teamB 0})
+  (fact  "Team A chains different points"
+         ((comp score-team-a score-team-a-3 score-team-a-2)) => {:teamA 6 :teamB 0})
 
-  )
-  
 )
