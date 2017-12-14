10f35e8530654f9f82ba98d412222a7615ece57a
Joaquin Caro <me@joaquin-caro.es>
2017-12-14 22:06:00 +0100

    Refactor: extract score incrementors

--- src/kata/core.clj
+++ src/kata/core.clj
@@ -1,24 +1,36 @@
 (ns kata.core)
 
-(defn start-match
-  []
-  {:local 0 :visitor 0})
+(defn start-match []  {:local 0 :visitor 0})
+
+(defn- inc-by
+  [by]
+  #(+ % by))
+
+(def one-point-to (inc-by 1))
+(def two-points-to (inc-by 2))
+(def three-points-to (inc-by 3))
+
 
 (defn local-scores-one-point
-   ([score] (assoc score :local (+ (:local score) 1))))
+   ([{local :local :as score}]
+    (assoc score :local (one-point-to local))))
 
 (defn local-scores-two-points
-  ([score] (assoc score :local (+ (:local score) 2))))
+  ([{local :local :as score}]
+   (assoc score :local (two-points-to local))))
 
 (defn local-scores-three-points
-  ([score] (assoc score :local (+ (:local score) 3))))
-
+  ([{local :local :as score}]
+   (assoc score :local (three-points-to local))))
 
 (defn visitor-scores-one-point
-  ([score] (assoc score :visitor (+ (:visitor score) 1))))
+  ([{visitor :visitor :as score}]
+   (assoc score :visitor (one-point-to visitor))))
 
 (defn visitor-scores-two-points
-  ([score] (assoc score :visitor (+ (:visitor score) 2))))
+  ([{visitor :visitor :as score}]
+   (assoc score :visitor (two-points-to visitor))))
 
 (defn visitor-scores-three-points
-  ([score] (assoc score :visitor (+ (:visitor score) 3))))
+  ([{visitor :visitor :as score}]
+   (assoc score :visitor (three-points-to visitor))))
